cb5af84345e9c351fc0058aae3b98b68b828a922
hadi <aljaami.hadi@gmail.com>
2025-11-26 14:48:25 +0300

    Initial commit: Responsive web template

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+// const toggleMenu = document.getElementById('toggleMenu');
+//       const navMenu = document.getElementById('navMenu');
+      
+//       toggleMenu.addEventListener('click', function() {
+//           navMenu.classList.toggle('show-menu');
+//       });
+      
+//       document.addEventListener('click', function(event) {
+//           if (!navMenu.contains(event.target) && !toggleMenu.contains(event.target)) {
+//               navMenu.classList.remove('show-menu');
+//           }
+//       });
+
+// --------------------------------------------------------------
+// Fade Up Animation on Scroll
+//--------------------------------------------------------------
+// document.addEventListener("DOMContentLoaded", function () {
+  // const fadeUpElements = document.querySelectorAll('.fade-up');
+  
+  // function checkScroll() {
+  //   fadeUpElements.forEach(element => {
+  //     const elementTop = element.getBoundingClientRect().top;
+  //     const elementVisible = 150;
+      
+  //     if (elementTop < window.innerHeight - elementVisible) {
+  //       element.classList.add('show');
+  //     } else {
+  //       element.classList.remove('show');
+  //     }
+  //   });
+  // }
+
+  // window.addEventListener('scroll', checkScroll);
+  // window.addEventListener('load', checkScroll);
+  
+  // checkScroll();
+// });
+
